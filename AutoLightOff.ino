1749d686c0cedf12cf7f9f70e2ebc95b1fc674ef
Sakuemy <pointd58@gmail.com>
2021-03-10 22:59:29 +0300

    Added blinking when changing settings. Update app. V0.87

--- AutoLightOff.ino
+++ AutoLightOff.ino
@@ -12,7 +12,7 @@ unsigned long timingSleap;
 unsigned long timingRele;
 unsigned long LongPress;
 bool Sleap = true;
-byte tap = 10;
+byte tap = 100;
 bool tapB = false;
 byte button = 4;       //Кнопка
 byte rele = 8;         //Реле
@@ -33,6 +33,7 @@ byte s = 0;            //Для энкодера
 int x = 0;             //Ответ от энкодера
 byte nkoder1 = 5;      //Энкодер пин 1
 byte nkoder2 = 8;      //Энкодер пин 2
+byte blinking = 0;
 
 void setup()
 {
@@ -49,7 +50,7 @@ void setup()
   lcd.setCursor(6, 0);  
   lcd.print("Load");
   for (int i = 0; i < 18; i++){
-    delay(150);
+    delay(100);
     lcd.setCursor(i, 1);  
     lcd.print("#");
   }
@@ -101,7 +102,7 @@ void loop()
         if (Settings == true){
           Settings = false;
           LongPressB = true;
-          tap = 10;
+          tap = 100;
           Sleap = true;
         } else {
           Settings = true;
@@ -125,7 +126,7 @@ void loop()
     }else{
       if ((digitalRead(button) == 0)and(tapB == true)and(Settings == true)){
         timingSleap = millis();
-        if (tap < 5){
+        if (tap < 7){
           tap++;
         }else{
           tap = 1;
@@ -138,7 +139,7 @@ void loop()
 /////////////////
 
 //Переключение реле
-  if (millis() - timingRele > 500){
+  if (millis() - timingRele > 3000){
     timingRele = millis();
     if ((analogRead(fRez) < MinLight)and(Light1 < MinLight)and(Light2 < MinLight)and(Light3 < MinLight)and(releB == false)){
       digitalWrite(rele, HIGH);
@@ -158,45 +159,105 @@ void loop()
 //Смена экранов
   if (millis() - timing > 250){
    timing = millis();
+   if (Settings == true){
+     if (blinking < 4){
+       blinking++;
+     }else{
+       blinking = 0;
+     }
+   }
    tmElements_t tm; 
     switch (tap) {
       case 1:
-        lcd.setCursor(0, 0);  
-        lcd.print("Light sensor:");
-        lcd.setCursor(0, 1);  
-        lcd.print(time0(String(analogRead(fRez)), 4));
-        break;
-      case 2:
         if (RTC.read(tm)) {
-          st = time0(String(tm.Hour), 2) + ":" + time0(String(tm.Minute), 2) + ":" + time0(String(tm.Second), 2);
+          if (blinking <= 2){
+            st = time0(String(tm.Hour), 2) + ":" + time0(String(tm.Minute), 2) + ":" + time0(String(tm.Second), 2);
+          }else{
+            st = "  :" + time0(String(tm.Minute), 2) + ":" + time0(String(tm.Second), 2);
+          }
           lcd.setCursor(0, 0);
           lcd.print("Time:");
           lcd.setCursor(0, 1);  
           lcd.print(st);
         } else {
-          lcd.setCursor(6, 1);
+          lcd.setCursor(0, 1);
+          lcd.print("Eror time");
+        }
+        break;
+      case 2:
+        if (RTC.read(tm)) {
+          if (blinking <= 2){
+            st = time0(String(tm.Hour), 2) + ":" + time0(String(tm.Minute), 2) + ":" + time0(String(tm.Second), 2);
+          }else{
+            st = time0(String(tm.Hour), 2) + ":  " + ":" + time0(String(tm.Second), 2);
+          }
+          lcd.setCursor(0, 0);
+          lcd.print("Time:");
+          lcd.setCursor(0, 1);  
+          lcd.print(st);
+        } else {
+          lcd.setCursor(0, 1);
           lcd.print("Eror time");
         }
         break;
       case 3:
-        lcd.setCursor(0, 0);  
-        lcd.print("TimeNight:");
-        lcd.setCursor(0, 1);  
-        lcd.print(time0(String(HourNight), 2) + ":" + time0(String(MinuteNight), 2));
+        if (RTC.read(tm)) {
+          if (blinking <= 2){
+            st = time0(String(tm.Hour), 2) + ":" + time0(String(tm.Minute), 2) + ":" + time0(String(tm.Second), 2);
+          }else{
+            st = time0(String(tm.Hour), 2) + ":" + time0(String(tm.Minute), 2) + ":  ";
+          }
+          lcd.setCursor(0, 0);
+          lcd.print("Time:");
+          lcd.setCursor(0, 1);  
+          lcd.print(st);
+        } else {
+          lcd.setCursor(0, 1);
+          lcd.print("Eror time");
+        }
         break;
       case 4:
         lcd.setCursor(0, 0);  
-        lcd.print("Min.Light:");
-        lcd.setCursor(0, 1);  
-        lcd.print(time0(String(MinLight), 4));
+        lcd.print("TimeNight:");
+        lcd.setCursor(0, 1);
+        if (blinking <= 2){
+          lcd.print(time0(String(HourNight), 2) + ":" + time0(String(MinuteNight), 2));
+        }else{
+          lcd.print("  :" + time0(String(MinuteNight), 2));
+        }
         break;
       case 5:
         lcd.setCursor(0, 0);  
+        lcd.print("TimeNight:");
+        lcd.setCursor(0, 1);
+        if (blinking <= 2){
+          lcd.print(time0(String(HourNight), 2) + ":" + time0(String(MinuteNight), 2));
+        }else{
+          lcd.print(time0(String(HourNight), 2) + ":  ");
+        }
+        break;
+      case 6:
+        lcd.setCursor(0, 0);  
+        lcd.print("Min.Light:");
+        lcd.setCursor(0, 1);
+        if (blinking <= 2){
+          lcd.print(time0(String(MinLight), 4));
+        }else{
+          lcd.print("    ");
+        }
+        break;
+      case 7:
+        lcd.setCursor(0, 0);  
         lcd.print("Max.Light:");
         lcd.setCursor(0, 1);  
-        lcd.print(time0(String(MaxLight), 4));
+        if (blinking <= 2){
+          lcd.print(time0(String(MaxLight), 4));
+        }else{
+          lcd.print("    ");
+        }
         break;
-      case 10:
+        
+      case 100:
         lcd.setCursor(0, 0);  
         lcd.print("Illum.:");
         lcd.setCursor(12, 0);  
